66fb8aba8ac62ba8ecca588295415dfd3f7b0529
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-10-30 09:27:14 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Item1 from "./Item1";
+import Item2 from "./Item2";
+import Item3 from "./Item3";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+    <h1>Item List</h1>
+    <ul>
+      <li>
+        <link to="/items/1">Item 1</link>
+      </li>
+      <li>
+        <link to="/items/2">Item 2</link>
+      </li>
+      <li>
+        <link to="/items/3">Item 3</link>
+      </li>
+    </ul>
+
+    <Routes>
+      <Route path="/items/:id" element={<Item1 />} />
+      <Route path="/items/:id" element={<Item2 />} />
+      <Route path="/items/:id" element={<Item3 />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Item2.js
@@ -0,0 +1,9 @@
+const Item2 = () => {
+    return (
+        <div>
+            <h1>Item 2</h1>
+            <p>Description for Item 2</p>
+        </div>
+    );
+}
+export default Item2;
